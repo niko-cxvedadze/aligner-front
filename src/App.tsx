620f7982e29be30c4ab991dc7307de5298e641a0
niko-cxvedadze <nikolozcxvedadze@gmail.com>
2024-06-10 00:15:46 +0400

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
-function App() {
-  return <></>;
-}
+import { Routes, Route } from "react-router-dom";
+import { AuthLayout } from "./layout/AuthLayout";
+import { SignIn, SignUp, useAuth } from "@clerk/clerk-react";
 
-export default App;
+export default function App() {
+  const { isSignedIn } = useAuth();
+
+  return (
+    <Routes>
+      {!isSignedIn && (
+        <Route path="/auth/*" element={<AuthLayout />}>
+          <Route path="sign-in" element={<SignIn signUpUrl="sign-up" />} />
+          <Route path="sign-up" element={<SignUp signInUrl="sign-in" />} />
+        </Route>
+      )}
+    </Routes>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,20 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "@/assets/global.css";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
   </React.StrictMode>
 );
