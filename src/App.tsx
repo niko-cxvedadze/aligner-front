b08a13576bfb4d1e032d802e1fb81a09e96c6283
niko-cxvedadze <nikolozcxvedadze@gmail.com>
2024-06-15 00:24:03 +0400

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
-import { Routes, Route, Navigate } from "react-router-dom";
-import { AuthLayout } from "./layout/AuthLayout";
-import { SignIn, SignUp, useAuth } from "@clerk/clerk-react";
+import {AuthLayout} from "./layout/AuthLayout";
+import {Routes, Route, Navigate} from "react-router-dom";
+import {SignIn, SignUp, useAuth} from "@clerk/clerk-react";
+import {useEffect} from "react";
 
 export default function App() {
-  const { isSignedIn, isLoaded } = useAuth();
+    const {isSignedIn, isLoaded, getToken} = useAuth();
 
-  return (
-    <Routes>
-      {!isSignedIn && isLoaded && (
-        <Route element={<AuthLayout />}>
-          <Route path="sign-in" element={<SignIn signUpUrl="sign-up" />} />
-          <Route path="sign-up" element={<SignUp signInUrl="sign-in" />} />
-          <Route path="*" element={<Navigate to="/sign-in" />} />
-        </Route>
-      )}
-    </Routes>
-  );
+    useEffect(() => {
+        (async () => {
+            const token = await getToken({template: "basic-token-v1"})
+            console.log(token);
+        })()
+    }, []);
+
+
+    return (
+        <Routes>
+            {!isSignedIn && isLoaded && (
+                <Route element={<AuthLayout/>}>
+                    <Route path="sign-in" element={<SignIn signUpUrl="sign-up"/>}/>
+                    <Route path="sign-up" element={<SignUp signInUrl="sign-in"/>}/>
+                    <Route path="*" element={<Navigate to="/sign-in"/>}/>
+                </Route>
+            )}
+        </Routes>
+    );
 }
