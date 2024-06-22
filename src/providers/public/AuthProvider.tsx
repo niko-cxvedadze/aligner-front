4270dcbd558131f00a8f9c76ea92a5e15fb58539
niko-cxvedadze <nikolozcxvedadze@gmail.com>
2024-06-22 23:30:51 +0400

    update-auth-flow

--- src/providers/public/AuthProvider.tsx
+++ src/providers/public/AuthProvider.tsx
@@ -1,25 +1,33 @@
 import { useSetAtom } from "jotai";
 import { authTokenAtom } from "./GlobalProvider";
 import { useAuth } from "@clerk/clerk-react";
-import { PropsWithChildren, useEffect } from "react";
+import { PropsWithChildren, useEffect, useState } from "react";
 import { privateAxios } from "@/utils/privateAxios.ts";
 
 export function AuthProvider({ children }: PropsWithChildren) {
+  const [loading, setLoading] = useState(true);
   const setAuthToken = useSetAtom(authTokenAtom);
   const { isLoaded, isSignedIn, getToken } = useAuth();
 
   async function fetchToken() {
-    const token = await getToken({ template: "basic-token-v1" });
-    setAuthToken(token);
-    console.log("Token fetched: ", token);
-    privateAxios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    try {
+      const token = await getToken({ template: "basic-token-v1" });
+      setAuthToken(token);
+      privateAxios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    } finally {
+      setLoading(false);
+    }
   }
 
   useEffect(() => {
     if (isLoaded && isSignedIn) {
       fetchToken();
+    } else if (isLoaded && !isSignedIn) {
+      setLoading(false);
     }
   }, [isLoaded, isSignedIn]);
+
+  if (loading) return <></>;
 
   return <>{children}</>;
 }
