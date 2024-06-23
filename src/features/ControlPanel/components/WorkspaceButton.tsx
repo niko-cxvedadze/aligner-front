d890d30917ae23ec890fb9748b8aa2128f279f57
niko-cxvedadze <nikolozcxvedadze@gmail.com>
2024-06-23 14:31:17 +0400

    new-components

--- src/features/ControlPanel/components/WorkspaceButton.tsx
+++ src/features/ControlPanel/components/WorkspaceButton.tsx
@@ -1,12 +1,35 @@
-import {ToggleGroupItem} from "@/components/ui/toggle-group.tsx";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu";
+import { TrashIcon } from "@radix-ui/react-icons";
+import { ToggleGroupItem } from "@/components/ui/toggle-group.tsx";
 
 interface WorkspaceButtonProps {
-    value: string
-    name: string
+  value: string;
+  name: string;
 }
 
-export function WorkspaceButton({name, value}: WorkspaceButtonProps) {
-    return <ToggleGroupItem value={value} className="w-[45px] h-[45px]" variant="outline">
-        {name.split('').slice(0, 2).join('').toUpperCase()}
-    </ToggleGroupItem>
+export function WorkspaceButton({ name, value }: WorkspaceButtonProps) {
+  return (
+    <ContextMenu>
+      <ContextMenuTrigger>
+        <ToggleGroupItem
+          value={value}
+          className="w-[45px] h-[45px]"
+          variant="outline"
+        >
+          {name.split("").slice(0, 2).join("").toUpperCase()}
+        </ToggleGroupItem>
+      </ContextMenuTrigger>
+      <ContextMenuContent>
+        <ContextMenuItem className="cursor-pointer text-destructive">
+          <TrashIcon className="mr-1.5" />
+          Delete
+        </ContextMenuItem>
+      </ContextMenuContent>
+    </ContextMenu>
+  );
 }
